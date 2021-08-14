adfacf376ac3ac3c57b2c92ad5f3dfdc3602b133
Nam Tran <namtvh@gmail.com>
2021-08-14 20:13:06 +0700

    Added content encryption

--- EncryptNote/Settings.m
+++ EncryptNote/Settings.m
@@ -18,7 +18,9 @@ NSString * const kBookmarksInAscOrder = @"BookmarksInAscOrder";
 
 @end
 
-@implementation Settings
+@implementation Settings {
+    NSString *_secKeyString;
+}
 
 + (Settings *)settings {
     static Settings *_sharedInstance = nil;
@@ -109,6 +111,50 @@ NSString * const kBookmarksInAscOrder = @"BookmarksInAscOrder";
         
         completionHandler(NO, authError.localizedDescription);
     }
+}
+
+- (NSString *)getSecKeyString {
+    if (_secKeyString == nil) {
+        _secKeyString = [[NSString alloc] initWithData:[self getSecKey] encoding:NSASCIIStringEncoding];
+    }
+    return _secKeyString;
+}
+
+- (NSData *)getSecKey {
+    // Identifier for our keychain entry - should be unique for your application
+    static const uint8_t kKeychainIdentifier[] = "com.technexity.EncryptNoteSharedSecKey";
+    NSData *tag = [[NSData alloc] initWithBytesNoCopy:(void *)kKeychainIdentifier
+                                               length:sizeof(kKeychainIdentifier)
+                                         freeWhenDone:NO];
+
+    // First check in the keychain for an existing key
+    NSDictionary *query = @{(__bridge id)kSecClass: (__bridge id)kSecClassKey,
+                            (__bridge id)kSecAttrApplicationTag: tag,
+                            (__bridge id)kSecAttrKeySizeInBits: @512,
+                            (__bridge id)kSecReturnData: @YES};
+
+    CFTypeRef dataRef = NULL;
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &dataRef);
+    if (status == errSecSuccess) {
+        return (__bridge NSData *)dataRef;
+    }
+
+    // No pre-existing key from this application, so generate a new one
+    uint8_t buffer[64];
+    status = SecRandomCopyBytes(kSecRandomDefault, 64, buffer);
+    NSAssert(status == 0, @"Failed to generate random bytes for key");
+    NSData *keyData = [[NSData alloc] initWithBytes:buffer length:sizeof(buffer)];
+
+    // Store the key in the keychain
+    query = @{(__bridge id)kSecClass: (__bridge id)kSecClassKey,
+              (__bridge id)kSecAttrApplicationTag: tag,
+              (__bridge id)kSecAttrKeySizeInBits: @512,
+              (__bridge id)kSecValueData: keyData};
+
+    status = SecItemAdd((__bridge CFDictionaryRef)query, NULL);
+    NSAssert(status == errSecSuccess, @"Failed to insert new key in the keychain");
+
+    return keyData;
 }
 
 #pragma mark - Alert & Action Sheet
